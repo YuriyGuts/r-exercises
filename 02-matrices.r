a21fe4f48aa823749af2cad00f7bb1d78ded45bb
Yuriy Guts <yuriy.guts@gmail.com>
2015-12-20 19:17:44 +0200

    Completed parts 2.4 through 2.8.

--- 02-matrices.r
+++ 02-matrices.r
@@ -21,3 +21,65 @@ crossprod(B)
 # 3
 matE <- matrix(0, nrow=6, ncol=6)
 ifelse(abs(row(matE) - col(matE)) == 1, 1, 0)
+
+
+# 4
+size <- 5
+outer(0:(size - 1), 0:(size - 1), "+")
+
+
+# 5a
+size <- 5
+outer(0:(size - 1), 0:(size - 1), "+") %% size
+
+
+# 5b
+size <- 10
+outer(0:(size - 1), 0:(size - 1), "+") %% size
+
+
+# 5c
+size <- 9
+outer(0:(size - 1), 0:(size - 1), "-") %% size
+
+
+# 6
+A <- abs(outer(1:5, 1:5, "-")) + 1
+y <- c(7, -1, -3, 5, 17)
+solve(A, y)
+
+
+# 7
+set.seed(75)
+aMat <- matrix(sample(10, size=60, replace=TRUE), nrow=6)
+
+
+# 7a
+rowSums(aMat > 4)
+
+
+# 7b
+which(rowSums(aMat == 7) == 2)
+
+
+# 7c
+columnSums <- colSums(aMat)
+which(outer(columnSums, columnSums, "+") > 75, arr.ind=TRUE)
+
+
+# 8a
+sum(outer(1:20, 1:5, function(i, j) {
+    i ^ 4 / (3 + j)
+}))
+
+
+# 8b
+sum(outer(1:20, 1:5, function(i, j) {
+    i ^ 4 / (3 + i * j)
+}))
+
+
+# 8c
+sum(outer(1:10, 1:10, function(i, j) {
+    ifelse(j <= i, i ^ 4 / (3 + i * j), 0)
+}))
